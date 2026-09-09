b9f0ed5f99a82ac471641f0b2a92a169cb7f85a3
Muhammad Rehman Rafi <153813023+M-Rehmans@users.noreply.github.com>
2026-09-09 23:51:33 +0500

    updated code of the App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,30 @@ function App() {
     lName: "",
     email: "",
   });
+  function handleContact(event) {
+    var { name, value } = event.target;
+    setContact((prev) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: contact.lName,
+          email: contact.email,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: contact.fName,
+          lName: value,
+          email: contact.email,
+        };
+      } else if (name === "email") {
+        return {
+          lName: contact.lName,
+          fName: contact.fName,
+          email: value,
+        };
+      }
+    });
+  }
 
   return (
     <div className="container">
@@ -14,9 +38,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+          onChange={handleContact}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleContact}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleContact}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
         <button>Submit</button>
       </form>
     </div>
